59b5548be08f2362f72686cf976ad4f9d7e268ab
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-03-09 17:21:22 -0400

    configuraçções de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+import Conteudo from './components/conteudo.jsx';
+import Header from './components/Header.jsx';
+import Objetivo from './components/Objetivo.jsx'
+import Footer from './components/Footer.jsx';
+import Mapa from './components/Mapa.jsx';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>ok eu bnão entendi</h1>
-    </>
-  )
+    <body>
+      <Header />
+      <Objetivo />
+      <Conteudo />
+      <Mapa />
+      <Footer />
+    </body>
+  );
 }
+
 
 export default App
